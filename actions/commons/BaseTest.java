cd883a7028eedf6e6cf720da58c28e2dd452b66a
MoTran <dream010895@gmail.com>
2022-09-16 16:04:01 +0700

    update

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -78,7 +78,47 @@ public class BaseTest {
 		return driver;
 	}
 
-	protected WebDriver getBrowserDriver(String browserName, String appUrl) {
+	protected WebDriver getBrowserDriver(String browserName, String environmentName) {
+		BrowserList browserList = BrowserList.valueOf(browserName.toUpperCase());
+		if (browserList == BrowserList.FIREFOX) {
+			System.out.println("Run firefox");
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+//			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (browserList == BrowserList.H_FIREFOX) {
+			WebDriverManager.firefoxdriver().setup();
+			FirefoxOptions options = new FirefoxOptions();
+			options.addArguments("--headless");
+			options.addArguments("window-size=1920x1080");
+			driver = new FirefoxDriver(options);
+		} else if (browserList == BrowserList.CHROME) {
+			System.out.println("Run Chrome");
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browserList == BrowserList.H_CHROME) {
+			System.out.println("Run Chrome");
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless");
+			options.addArguments("window-size=1920x1080");
+			driver = new ChromeDriver(options);
+		} else if (browserList == BrowserList.EDGE) {
+			System.out.println("Run edge");
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		} else if (browserList == BrowserList.SAFARI) {
+			System.out.println("Run SAFARI");
+			WebDriverManager.safaridriver().setup();
+			driver = new SafariDriver();
+		} else {
+			throw new RuntimeException("Browser name invalid.");
+		}
+		driver.manage().timeouts().implicitlyWait(GlobalConstants.LONG_TIMEOUT, TimeUnit.SECONDS);
+		driver.get(getEnvironmentUrl(environmentName));
+		return driver;
+	}
+	
+	protected WebDriver getBrowserDriverByAppUrl(String browserName, String appUrl) {
 		BrowserList browserList = BrowserList.valueOf(browserName.toUpperCase());
 		if (browserList == BrowserList.FIREFOX) {
 			System.out.println("Run firefox");
@@ -124,13 +164,26 @@ public class BaseTest {
 
 	}
 
-	protected String getEnvironmentUrl(String serverName) {
+	protected String getEnvironmentUrl(String environmentName) {
 		String envUrl = null;
-		EnvironmentList environment = EnvironmentList.valueOf(serverName.toUpperCase());
-		if (environment == EnvironmentList.DEV) {
+		EnvironmentList environment = EnvironmentList.valueOf(environmentName.toUpperCase());
+		switch (environment) {
+		case DEV:
 			envUrl = "https://demo.nopcommerce.com";
-		} else if (environment == EnvironmentList.STAGING) {
-			envUrl = "https://admin-demo.nopcommerce.com";
+			break;
+		case TESTING:
+			envUrl = "https://testing.nopcommerce.com";
+			break;
+		case STAGING:
+			envUrl = "https://staging.nopcommerce.com";
+			break;
+		case PRODUCTION:
+			envUrl = "https://prod.nopcommerce.com";
+			break;
+
+		default:
+			envUrl = null;
+			break;
 		}
 		return envUrl;
 	}
---
+++ testcases/com/nopcommerce/user/Level_20_Multiple_Environment_Owner.java
@@ -0,0 +1,126 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
+import com.nopcommerce.data.UserDataMapper;
+
+import commons.BaseTest;
+import commons.PageGeneratorManager;
+import pageObjects.nopcommerce.user.UserCustomerInforPageObject;
+import pageObjects.nopcommerce.user.UserHomePageObject;
+import pageObjects.nopcommerce.user.UserLoginPageObject;
+
+import pageObjects.nopcommerce.user.UserRegisterPageObject;
+import pageUIs.nopcommerce.user.BasePageUI;
+import utilities.DataHelper;
+import utilities.Environment;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.WebDriver;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class Level_20_Multiple_Environment_Owner extends BaseTest {
+	Environment environment;
+	@Parameters({"browser","environment"})
+	@BeforeClass
+	public void beforeClass(String browserName, String environmentName) {
+		ConfigFactory.setProperty("env", environmentName);
+		environment = ConfigFactory.create(Environment.class);
+		
+		driver = getBrowserDriverByAppUrl(browserName, environment.appUrl());
+		homePage = PageGeneratorManager.getUserHomePage(driver);
+		userData = UserDataMapper.getUserData();
+		email = userData.getEmailAddress()  + generateFakeNumber() + "@hotmail.com";
+
+	}
+
+	@Test
+	public void User_01_Register() {
+		log.info("Register - step 01: open register page");
+		registerPage = homePage.openRegisterPage();
+		
+		registerPage.clickToRadioButtonByLabel(driver,"Female");
+		log.info("Register - step 02: enter to firstname textbox with value is '" + userData.getFirstName() + "'");
+		registerPage.inputToTextboxByID(driver, "FirstName", userData.getFirstName());
+
+		log.info("Register - step 03: enter to lastName textbox with value is '" + userData.getLastName() + "'");
+		registerPage.inputToTextboxByID(driver, "LastName", userData.getLastName());
+
+		log.info("Register - step 04: enter to email textbox with value is '" + email + "'");
+		registerPage.inputToTextboxByID(driver, "Email", email);
+		
+		registerPage.clickToCheckboxByLabel(driver,"Newsletter");
+
+
+		log.info("Register - step 05: enter to password textbox with value is '" + userData.getPassword() + "'");
+		registerPage.inputToTextboxByID(driver, "Password", userData.getPassword());
+		registerPage.selectToDropdownByName(driver, "DateOfBirthDay", userData.getDate());
+		registerPage.selectToDropdownByName(driver, "DateOfBirthMonth", userData.getMonth());
+		registerPage.selectToDropdownByName(driver, "DateOfBirthYear", userData.getYear());
+
+		log.info("Register - step 06: enter to password textbox with value is '" + userData.getPassword() + "'");
+
+		registerPage.inputToTextboxByID(driver, "ConfirmPassword", userData.getPassword());
+		log.info("Register - step 07: click register button");
+		registerPage.clickToButtonByText(driver, "Register");
+
+		log.info("Register - step 08: verify register success message is displayed ");
+		Assert.assertEquals(registerPage.getRegisterSucessMessage(), "Your registration completed");
+
+	}
+
+	@Test
+	public void User_02_Login() {
+		log.info("Register - step 09: click logout button");
+		homePage = registerPage.clickToLogoutLink();
+		log.info("Login - step 01: open login page");
+		loginPage = homePage.openLoginPage();
+		log.info("Login - step 02: enter email to textbox with value is '" + email + "'");
+		loginPage.inputToTextboxByID(driver, "Email", email);
+		log.info("Login - step 03: enter password to textbox with value is '" + userData.getPassword() + "'");
+		loginPage.inputToTextboxByID(driver, "Password", userData.getPassword());
+
+		log.info("Login - step 04: click login button");
+		loginPage.clickToButtonByText(driver, "Log in");
+		homePage = PageGeneratorManager.getUserHomePage(driver);
+		log.info("Login - step 05: Verify 'My Account' link is displayed");
+		Assert.assertTrue(homePage.isMyAccountLinkDisplayed());
+		
+	}
+	@Test
+	public void User_03_My_Account() {
+		log.info("My Account - step 01: Navigate 'My Account' page");
+		customerInforPage = homePage.openMyAccountPage();
+		log.info("My Account - step 02: Verify 'Customer Page' link is displayed");
+		Assert.assertTrue(customerInforPage.isCustomerInforPageDisplayed());
+		customerInforPage.sleepInSecond(10);
+		log.info("My Account - step 03: Verify 'First Name' value is correctly");
+		Assert.assertEquals(customerInforPage.getTextboxValueByID(driver,"FirstName"), userData.getFirstName());
+		log.info("My Account - step 04: Verify 'Last Name' value is correctly");
+		Assert.assertEquals(customerInforPage.getTextboxValueByID(driver,"LastName"), userData.getLastName());
+		log.info("My Account - step 05: Verify 'Email' value is correctly");
+		Assert.assertEquals(customerInforPage.getTextboxValueByID(driver,"Email"), email);
+
+
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void afterClass() {
+		closeBrowserAndDriver();
+	}
+
+	private WebDriver driver;
+	private String email;
+	private UserHomePageObject homePage;
+	private UserRegisterPageObject registerPage;
+	private UserLoginPageObject loginPage;
+	private UserCustomerInforPageObject customerInforPage;
+	UserDataMapper userData;
+
+
+}
